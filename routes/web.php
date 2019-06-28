5acd85925e0c2e75760864b5b0eb4a9ccf69d73a
Annei <edelyav@gmail.com>
2019-06-27 21:22:35 -0500

    archivos

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','UploadController@index');
+
+Route::post('/store','UploadController@store')->name('upload.file');
+
+Route::get('/show','UploadController@show');
+
